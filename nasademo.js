e0fd29c0da6699a42ea2d842328aa862ed512a55
Mika Stenberg <mika.stenberg@laurea.fi>
2021-02-11 10:36:48 +0200

    Add files via upload

---
+++ nasademo.js
@@ -0,0 +1,46 @@
+const request = require("request");
+var http = require("http");
+var data;
+const PORT = process.env.PORT || 3000;
+
+request(
+  "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY",
+  { json: true },
+  (err, res, body) => {
+    if (err) {
+      return console.log(err);
+    }
+    data = body;
+    // console.log(res);
+    // console.log(body);
+    // console.log(body.url);
+    // console.log(body.explanation);
+  }
+);
+
+function parse(data) {
+  var html = "<table>";
+
+  html += "<tr><td>" + data.title + "</td></tr>";
+  html += "<tr><td>" + data.explanation + "</td></tr>";
+  html += "<tr><td><img src='" + data.url + "'></td></tr>";
+
+  html += "</table>";
+  console.log(html);
+  return html;
+}
+
+//create a server object:
+http
+  .createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+
+    response.write("<table border='1'>");
+    response.write("<tr><td>" + data.title + "</td></tr>");
+    response.write("<tr><td>" + data.explanation + "</td></tr>");
+    response.write("<tr><td><img src='" + data.url + "'></td></tr>");
+    response.write("</table>");
+
+    response.end(); //end the response
+  })
+  .listen(PORT); //the server object listens on port 3000
